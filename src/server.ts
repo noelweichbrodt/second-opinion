c514229edaefe8f1b642d2afa4d0dfa74f17a49c
Noel Weichbrodt <noel@weichbrodt.org>
2026-03-20 13:58:13 -0400

    feat(ci): release-it flow

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ export function createServer(): Server {
   const server = new Server(
     {
       name: "second-opinion",
-      version: "0.1.0",
+      version: "0.5.1",
     },
     {
       capabilities: {
